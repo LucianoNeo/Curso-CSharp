b0ae644ac7c73d516cebb3aab050c44383327929
Luciano Bueno <luciano.bueno@ingacode.com.br>
2023-03-08 17:31:31 -0300

    projeto 7 adicionado

---
+++ 6-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 6 - Atribuições de variáveis");
+
+        int idade = 30;
+        int idadeAna = idade;
+        idade = 25;
+        Console.WriteLine(idadeAna);// imprime 30
+
+        Console.WriteLine("Tecle ENTER para fechar...");
+        Console.ReadLine();
+    }
+
+}
---
+++ 7-Condicionais/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 7 - Condicionais");
+        int idadeJoao = 16;
+        int quantidadePessoas = 2;
+
+        if (idadeJoao >= 18)
+        {
+            Console.WriteLine("Pode Entrar!");
+        }
+        else
+        {
+            if (quantidadePessoas > 0)
+            {
+                Console.WriteLine("Ele está acompanhado. Pode Entrar!");
+            }
+            else
+            {
+                Console.WriteLine("Não pode Entrar!");
+            }
+        }
+
+
+        Console.WriteLine("Tecle ENTER para fechar...");
+        Console.ReadLine();
+    }
+
+}
