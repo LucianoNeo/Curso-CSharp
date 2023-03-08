ec7e6a543992472d90ee1b37605b0cd8d0d6c689
Luciano Bueno <luciano.bueno@ingacode.com.br>
2023-03-08 16:46:07 -0300

    Projeto 4 adicionado

---
+++ 4-ConversoesEOutrosTipos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 4 - Conversões e outros tipos");
+        
+        double salario = 3000.15;
+        Console.WriteLine(salario);
+
+        int salarioInteiro = (int)salario;
+        Console.WriteLine(salarioInteiro);
+
+        long x = 200000000000000000;
+        Console.WriteLine(x);
+
+        short y = 15000;
+        Console.WriteLine(y);
+
+        float altura = 1.62f;
+        Console.WriteLine(altura);
+
+        Console.WriteLine("Tecle ENTER para fechar...");
+        Console.ReadLine();
+    }
+
+}
