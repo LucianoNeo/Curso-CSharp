31fd4225c563cf88a1c6bed94e7d3417ccb44566
Luciano Bueno <luciano.bueno@ingacode.com.br>
2023-03-08 17:14:20 -0300

    Projeto 5 adicionado

---
+++ 5-CaracteresETextos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 5 - Caracteres e Textos");
+
+        char letra = 'a'; // armazena 1 caractere
+        Console.WriteLine(letra);
+
+        letra = (char)65; //imprime 'A' => conversão de decimal para char (consultar tabela ASCII)
+        Console.WriteLine(letra);
+
+
+        string primeiraFrase = "Alura - cursos de tecnologia";
+        //para quebra de linha em string adicionar @
+        string cursos = @"Cursos disponíveis:
+- Go
+- C#
+- Python
+- Java";
+        Console.WriteLine(primeiraFrase);
+        Console.WriteLine(cursos);
+
+        Console.WriteLine("Tecle ENTER para fechar...");
+        Console.ReadLine();
+    }
+
+}
