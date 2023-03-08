90fe27c761008fa5de0ac675b94b998c5753cdb3
Luciano Bueno <luciano.bueno@ingacode.com.br>
2023-03-08 16:30:56 -0300

    Projeto 3 adicionado

---
+++ 3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 3 - Variáveis Ponto Flutuante");
+
+        double salario = 10200.50 / 2 ;
+        
+        Console.WriteLine("Meu salário é " + salario);
+        Console.WriteLine("Tecle ENTER para fechar...");
+        Console.ReadLine();
+    }
+
+}
