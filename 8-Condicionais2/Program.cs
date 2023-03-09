3d6f596b0f681d3c9ccd2278c5c70201ff3908c2
Luciano Bueno <luciano.bueno@ingacode.com.br>
2023-03-09 09:10:42 -0300

    projeto 9 adicionado

---
+++ 8-Condicionais2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 7 - Condicionais");
+
+        int idadeJoao = 16;
+        int quantidadePessoas = 2;
+
+        bool acompanhado = quantidadePessoas > 1;
+        bool grupo = true;
+
+        if (idadeJoao >= 18 || acompanhado)
+        {
+            Console.WriteLine("Pode Entrar!");
+        }
+        else
+        {
+            
+            Console.WriteLine("Não pode Entrar!");
+            
+        }
+
+
+        Console.WriteLine("Tecle ENTER para fechar...");
+        Console.ReadLine();
+    }
+
+}
---
+++ 9-Escopo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 9 - Escopo");
+
+        int idadeJoao = 16;
+        int quantidadePessoas = 2;
+
+        bool acompanhado = quantidadePessoas > 1;
+        string textoAdicional;
+
+        if (acompanhado)
+        {
+           textoAdicional = "João está acompanhado";
+            Console.WriteLine(textoAdicional);
+            Console.WriteLine("Pode Entrar!");
+        }
+        else
+        {
+            textoAdicional = "João não está acompanhado";
+            Console.WriteLine(textoAdicional);
+            Console.WriteLine("Não pode Entrar!");
+
+        }
+
+
+        Console.WriteLine("Tecle ENTER para fechar...");
+        Console.ReadLine();
+    }
+
+}
