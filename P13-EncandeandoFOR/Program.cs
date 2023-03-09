459c53fe3f6dbe1553f62858470ca048604a9777
Luciano Bueno <luciano.bueno@ingacode.com.br>
2023-03-09 09:50:26 -0300

    projeto 13 adicionado

---
+++ P13-EncandeandoFOR/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 13 - Encadeando FOR");
+
+        //*
+        //**
+        //***
+        //****
+        //*****
+
+
+        // com break
+        for(int contadorLinhas = 0; contadorLinhas < 10; contadorLinhas++)
+        {
+            for (int contadorColunas = 0; contadorColunas <= 10; contadorColunas++)
+            {
+                Console.Write('*');
+                if (contadorColunas >= contadorLinhas) break;
+            }
+                Console.WriteLine();
+        }
+
+
+        // sem break
+        for (int contadorLinhas = 0; contadorLinhas < 10; contadorLinhas++)
+        {
+            for (int contadorColunas = 0; contadorColunas <= contadorLinhas; contadorColunas++)
+            {
+                Console.Write('*');
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Tecle ENTER para fechar...");
+        Console.ReadLine();
+    }
+
+}
