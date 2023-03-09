309ccfcadae5f0dda68e85b5934f2849c98ecef9
Luciano Bueno <luciano.bueno@ingacode.com.br>
2023-03-09 09:27:09 -0300

    Projeto 10 adicionado

---
+++ P10-CalculaPoupanca/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 10 - Calcula Poupança");
+
+        double investimento = 1000;
+
+        double rendimento = investimento * 0.005;
+
+        int mes = 1;
+        
+
+        while (mes <= 12)
+        {
+            investimento = investimento + rendimento;
+            Console.WriteLine("No mês "+mes + " você terá R$:" + investimento);
+            mes++;
+        }
+
+
+        Console.WriteLine("Tecle ENTER para fechar...");
+        Console.ReadLine();
+    }
+
+}
